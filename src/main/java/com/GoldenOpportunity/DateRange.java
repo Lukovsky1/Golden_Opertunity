29ed476f87b528418484f634f3662ac0f617a5f9
Aubrey Eman Bedford <Blaze@Aubreys-MacBook-Pro.local>
2026-03-21 21:13:26 -0500

    Adding DateRange implementation

--- src/main/java/com/GoldenOpportunity/DateRange.java
+++ src/main/java/com/GoldenOpportunity/DateRange.java
@@ -1,4 +1,10 @@
 package com.GoldenOpportunity;
 
-public class DateRange {
+import java.time.LocalDate;
+
+//Used to check overlaps between reservations
+public record DateRange(LocalDate startDate, LocalDate endDate) {
+    public boolean overlaps(DateRange other) {
+        return this.startDate.isBefore(other.endDate) && other.startDate.isBefore(this.endDate);
+    }
 }
