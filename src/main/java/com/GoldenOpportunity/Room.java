b7b9d334c6b1927f124748e60024e76a1a7db873
Aubrey Eman Bedford <Blaze@Aubreys-MacBook-Pro.local>
2026-03-24 21:38:01 -0500

    Adding Griffin's implementation of room to brahc

--- src/main/java/com/GoldenOpportunity/Room.java
+++ src/main/java/com/GoldenOpportunity/Room.java
@@ -1,4 +1,73 @@
 package com.GoldenOpportunity;
 
 public class Room {
+    int roomNo;
+    int beds;
+    boolean smoking;
+    String qLevel;
+    String roomType;
+    double rate;
+
+    Room(int rmNo, int b, boolean sm, String qlty, String rmType, double r){
+        roomNo = rmNo;
+        beds = b;
+        smoking = sm;
+        qLevel = qlty;
+        roomType = rmType;
+        rate = r;
+    }
+
+    public int getRoomNo() { return roomNo; }
+    public int getBeds() { return beds; }
+    public boolean isSmoking() { return smoking; }
+    public String getQLevel() { return qLevel; }
+    public String getRoomType() { return roomType; }
+    public double getRate() { return rate; }
+
+    public void setBeds(int beds) { this.beds = beds; }
+    public void setSmoking(boolean smoking) { this.smoking = smoking; }
+    public void setQLevel(String qLevel) { this.qLevel = qLevel; }
+    public void setRoomType(String roomType) { this.roomType = roomType; }
+    public void setRate(double rate) { this.rate = rate; }
+
+    String [] bedTypes = {"King", "Queen", "Twin", "Full"};
+
+
+    public enum QualityLevel{
+        Economy, Comfort, Business, Executive;
+    }
 }
+
+/*
+public class Room {
+    int roomNo;
+    int beds;
+    boolean smoking;
+    String qLevel;
+    String roomType;
+    double rate;
+
+    Room(int rmNo, int b, boolean sm, String qlty, String rmType, double r){
+        roomNo = rmNo;
+        beds = b;
+        smoking = sm;
+        qLevel = qlty;
+        roomType = rmType;
+        rate = r;
+    }
+
+    public int getRoomNo() { return roomNo; }
+    public int getBeds() { return beds; }
+    public boolean isSmoking() { return smoking; }
+    public String getQLevel() { return qLevel; }
+    public String getRoomType() { return roomType; }
+    public double getRate() { return rate; }
+
+    public void setBeds(int beds) { this.beds = beds; }
+    public void setSmoking(boolean smoking) { this.smoking = smoking; }
+    public void setQLevel(String qLevel) { this.qLevel = qLevel; }
+    public void setRoomType(String roomType) { this.roomType = roomType; }
+    public void setRate(double rate) { this.rate = rate; }
+
+}
+ */
