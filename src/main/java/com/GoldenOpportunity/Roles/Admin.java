94054df7c0e1f9fc62494797816aa6c6e867a9e5
Lucas Ulibarri <lucas_ulibarri2@baylor.edu>
2026-03-25 12:27:57 -0500

    Editied the user roles to conform to the User class set up. Also, put the roles in their own package for organization.

---
+++ src/main/java/com/GoldenOpportunity/Roles/Admin.java
@@ -0,0 +1,11 @@
+package com.GoldenOpportunity.Roles;
+
+import com.GoldenOpportunity.User;
+import com.GoldenOpportunity.enums.Role;
+
+public class Admin extends User {
+
+    public Admin(int id, String username, String password, String contactInfo) {
+        super(id, username, password, contactInfo, Role.ADMIN);
+    }
+}
