1c5a516ae694e8219fcd38c8cb1b7177c3fb457a
Habiba <elyousfihabiba0@gmail.com>
2026-04-28 20:17:41 -0500

    Add cart backend files from databaseStandardization

---
+++ src/main/java/com/GoldenOpportunity/Shop/ProductRepo.java
@@ -0,0 +1,70 @@
+package com.GoldenOpportunity.Shop;
+
+import com.GoldenOpportunity.DatabaseTools.DBUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+// this file is used to work w all the SQL stuff
+//FIXME: Edited this file to not take a url and instead use the DBUtil.getConnection() url
+public class ProductRepo {
+
+    public ProductRepo() {}
+
+    public List<ProductDescription> getAllProductDescriptions() {
+        List<ProductDescription> productDescriptions = new ArrayList<>();
+
+        String sql = "SELECT productID, name, price, image, description FROM ProductDescriptions";
+
+        try (Connection connection = DBUtil.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+                ProductDescription productDescription = new ProductDescription(
+                        resultSet.getDouble("price"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("productID"),
+                        resultSet.getString("image"),
+                        resultSet.getString("description")
+                );
+
+                productDescriptions.add(productDescription);
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("error loading product descriptions from database", e);
+        }
+
+        return productDescriptions;
+    }
+
+    public List<ProductInventory> getAllProductInventory() {
+        List<ProductInventory> inventoryList = new ArrayList<>();
+
+        String sql = "SELECT productID, stock FROM ProductDescriptions";
+
+        try (Connection connection = DBUtil.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+
+            while (resultSet.next()) {
+                ProductInventory inventory = new ProductInventory(
+                        resultSet.getInt("productID"),
+                        resultSet.getInt("stock")
+                );
+
+                inventoryList.add(inventory);
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException("error loading product inventory from database", e);
+        }
+
+        return inventoryList;
+    }
+}
